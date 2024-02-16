e2ecd7205a79cdde5b4abc21d40f4e9735e083ea
Ragnarloki <sivasankarswaminathan62@gmail.com>
2024-02-16 12:41:32 +0530

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Signup from './Signup'
+import Login from './Login'
+import Home from './Home'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div>App</div>
-  )
+  
+   <Login />
+   )
 }
 
 export default App
