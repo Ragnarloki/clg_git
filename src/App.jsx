a9f64f007058027ffdb58c46adc7bc200c908a63
Ragnarloki <sivasankarswaminathan62@gmail.com>
2024-02-23 11:42:41 +0530

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
-import Signup from './Signup'
-import Login from './Login'
+
 import Home from './Home'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
