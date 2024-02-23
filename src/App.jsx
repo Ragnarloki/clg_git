6c85f67be73948da681616de7539d062662b3963
Ragnarloki <sivasankarswaminathan62@gmail.com>
2024-02-23 11:55:41 +0530

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
-import "bootstrap/dist/css/bootstrap.min.css"
-
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "jquery/dist/jquery.min.js"
+import 'bootstrap/dist/js/bootstrap.min.js'
+import Card from './components/Card'
 import Home from './Home'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import contents from '../contents'
 
+
+function createCard(content){
+  return (<Card 
+    key={content.id}
+    name={content.fruitname}          
+    image={content.img}/>
+  );  
+}
 function App() {
   return (
-  
-    <Home/>
-   )
+    <div>
+      <Home/>
+      {contents.map(createCard)}
+      
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Card(props) {
+  return (
+    
+    <div className="container">
+      <div className='row'>
+        <div className='col-md-3 '>
+
+        <div className=" card ">
+         <img src={props.image} className="card-img-top" height={'200px'} width={'170px'}  />
+             <div className="card-body">
+               <h5 className="card-title justify d-flex justify-content-center">{props.name}</h5>
+              
+             </div>
+        
+             </div>
+      </div>
+        </div>
+      </div>
+  )
+}
+
+export default Card
